dce47585304030d1f05e2a87002f5ae85fc8849e
Eric Dasse <christ.gououi@gmail.com>
2023-02-12 20:34:45 +0100

    update_quality: Test normal items quality decrease

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,8 +10,29 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
-        
+
         assert items[0].name == "foo"
+
+    def test_update_quality_should_decrease_quality_and_sell_in_when_it_is_a_normal_item(
+        self,
+    ):
+        normal_items = [
+            Item(name="foo", sell_in=12, quality=13),
+            Item(name="bar", sell_in=14, quality=4),
+            Item(name="foobar", sell_in=2, quality=45),
+        ]
+        gilded_rose = GildedRose(normal_items)
+        initial_qualities = [item.quality for item in normal_items]
+        initial_sell_ins = [item.sell_in for item in normal_items]
+
+        gilded_rose.update_quality()
+
+        for i, item in enumerate(normal_items):
+            former_quality = initial_qualities[i]
+            former_sell_in = initial_sell_ins[i]
+
+            assert item.quality == former_quality - 1
+            assert item.sell_in == former_sell_in - 1
 
 
 if __name__ == "__main__":
