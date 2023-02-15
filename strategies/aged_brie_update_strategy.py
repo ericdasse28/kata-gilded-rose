89afcdc1aaeb930e71f04956e1fbf1a29459464e
Eric Dasse <christ.gououi@gmail.com>
2023-02-15 16:56:26 +0100

    Feat: Implement aged brie strategy

--- strategies/aged_brie_update_strategy.py
+++ strategies/aged_brie_update_strategy.py
@@ -3,4 +3,11 @@ from gilded_rose.item_update_strategy import ItemUpdateStategy
 
 class AgedBrieItemUpdateStrategy(ItemUpdateStategy):
     def update_quality(self, item):
+        if item.sell_in <= 0:
+            item.quality = min(item.quality + 2, self.MAX_QUALITY)
+        else:
+            item.quality = min(item.quality + 1, self.MAX_QUALITY)
+
+        item.sell_in -= 1
+
         return item
