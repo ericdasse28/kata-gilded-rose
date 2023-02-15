e1d0b91aa769d5cbe0f072b5ffcfbb3228cbd1b2
Eric Dasse <christ.gououi@gmail.com>
2023-02-15 16:56:44 +0100

    Feat: Implement backstage passes strategy

--- strategies/backstage_passes_update_strategy.py
+++ strategies/backstage_passes_update_strategy.py
@@ -3,4 +3,15 @@ from gilded_rose.item_update_strategy import ItemUpdateStategy
 
 class BackstagePassesItemUpdateStrategy(ItemUpdateStategy):
     def update_quality(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality = min(item.quality + 3, self.MAX_QUALITY)
+        elif item.sell_in <= 10:
+            item.quality = min(item.quality + 2, self.MAX_QUALITY)
+        else:
+            item.quality = min(item.quality + 1, self.MAX_QUALITY)
+
+        item.sell_in -= 1
+
         return item
