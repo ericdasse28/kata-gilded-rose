a9cb7e8ca3a97fe9a96aa6e0b177627fc548bd6d
Eric Dasse <christ.gououi@gmail.com>
2023-02-15 16:09:06 +0100

    Fix: remove Item import
    This prevents circular import problems in `gilded_rose.py`

--- gilded_rose/item_update_strategy.py
+++ gilded_rose/item_update_strategy.py
@@ -1,12 +1,10 @@
 from abc import ABCMeta, abstractmethod
 
-from gilded_rose.gilded_rose import Item
-
 
 class ItemUpdateStategy(metaclass=ABCMeta):
     MIN_QUALITY = 0
     MAX_QUALITY = 50
 
     @abstractmethod
-    def update_quality(item: Item) -> Item:
+    def update_quality(item):
         pass
--- gilded_rose/item_updater.py
+++ gilded_rose/item_updater.py
@@ -1,10 +1,9 @@
-from gilded_rose.gilded_rose import Item
-from item_update_strategy import ItemUpdateStategy
+from gilded_rose.item_update_strategy import ItemUpdateStategy
 
 
 class ItemUpdater:
     def __init__(self, item_update_strategy: ItemUpdateStategy):
         self.item_update_strategy = item_update_strategy
 
-    def update_quality(self, item: Item) -> Item:
+    def update_quality(self, item):
         return self.item_update_strategy.update_quality(item)
