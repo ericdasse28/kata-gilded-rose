13b64f08e58710c8fda9699c9a06aa633b3976ba
Eric Dasse <christ.gououi@gmail.com>
2023-02-16 07:06:48 +0100

    Feat: add test class for backstage passes

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -330,6 +330,113 @@ class TestSulfuraItems:
         assert actual_quality == initial_quality
 
 
+class TestBackstagePasses:
+    @pytest.mark.parametrize("initial_sell_in", [6, 7, 8, 0, -2])
+    def test_sell_in_should_decrease(self, initial_sell_in):
+        gilded_rose = GildedRose(
+            [Item(name="Conjured Mana Cake", sell_in=initial_sell_in, quality=2)]
+        )
+
+        gilded_rose.update_quality()
+
+        actual_sell_in = gilded_rose.items[0].sell_in
+        assert actual_sell_in == initial_sell_in - 1
+
+    @pytest.mark.parametrize(
+        "sell_in, initial_quality",
+        [(11, 8), (15, 7), (13, 6), (17, 9), (25, 2), (14, 49)],
+    )
+    def test_quality_should_increase_by_1_after_update_when_sell_in_is_above_10(
+        self, sell_in, initial_quality
+    ):
+        gilded_rose = GildedRose(
+            [
+                Item(
+                    name="Backstage passes to a TAFKAL80ETC concert",
+                    sell_in=sell_in,
+                    quality=initial_quality,
+                )
+            ]
+        )
+
+        gilded_rose.update_quality()
+
+        actual_quality = gilded_rose.items[0].quality
+        assert actual_quality == initial_quality + 1
+
+    @pytest.mark.parametrize(
+        "sell_in, initial_quality",
+        [(10, 8), (6, 7), (7, 6), (8, 9), (9, 2), (7, 48)],
+    )
+    def test_quality_should_increase_by_2_after_update_when_sell_in_is_between_6_and_10(
+        self, sell_in, initial_quality
+    ):
+        gilded_rose = GildedRose(
+            [
+                Item(
+                    name="Backstage passes to a TAFKAL80ETC concert",
+                    sell_in=sell_in,
+                    quality=initial_quality,
+                )
+            ]
+        )
+
+        gilded_rose.update_quality()
+
+        actual_quality = gilded_rose.items[0].quality
+        assert actual_quality == initial_quality + 2
+
+    @pytest.mark.parametrize(
+        "sell_in, initial_quality",
+        [(5, 8), (4, 7), (3, 6), (2, 9), (1, 2), (3, 47)],
+    )
+    def test_quality_should_increase_by_3_after_update_when_sell_in_less_than_5(
+        self, sell_in, initial_quality
+    ):
+        gilded_rose = GildedRose(
+            [
+                Item(
+                    name="Backstage passes to a TAFKAL80ETC concert",
+                    sell_in=sell_in,
+                    quality=initial_quality,
+                )
+            ]
+        )
+
+        gilded_rose.update_quality()
+
+        actual_quality = gilded_rose.items[0].quality
+        assert actual_quality == initial_quality + 3
+
+    @pytest.mark.parametrize(
+        "sell_in, initial_quality",
+        [(0, 8), (-1, 7), (-3, 6), (-2, 9), (-11, 2), (-15, 47)],
+    )
+    def test_quality_drops_to_0_after_concert(self, sell_in, initial_quality):
+        gilded_rose = GildedRose(
+            [
+                Item(
+                    name="Backstage passes to a TAFKAL80ETC concert",
+                    sell_in=sell_in,
+                    quality=initial_quality,
+                )
+            ]
+        )
+
+        gilded_rose.update_quality()
+
+        actual_quality = gilded_rose.items[0].quality
+        assert actual_quality == 0
+
+    def test_quality_is_never_above_50(self):
+        gilded_rose = GildedRose([Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=7, quality=50)])
+
+        gilded_rose.update_quality()
+
+        actual_quality = gilded_rose.items[0].quality
+        assert actual_quality == 50
+
+
 class TestConjuredItems:
     @pytest.mark.parametrize("initial_sell_in", [6, 7, 8, 0, -2])
     def test_sell_in_should_decrease(self, initial_sell_in):
